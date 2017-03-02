f90a965b6710bb3f2763086f646b9d85d3625002
fslic <fslic@DESKTOP-MCATCHJ>
2017-03-02 03:29:02 -0800

    new demo

--- MyBatis_XML/src/main/java/evolution/dao/TheOtherDao.java
+++ MyBatis_XML/src/main/java/evolution/dao/TheOtherDao.java
@@ -7,8 +7,10 @@ import org.apache.ibatis.annotations.Param;
 import evolution.entity.TheOtherEntity;
 
 public interface TheOtherDao {
+	// @Param("name") corresponds to #{name} in theOtherEntity.xml
 	public List<TheOtherEntity> selectByNameAndAge(@Param("name") String name, @Param("age") int age);
 	
+	// theOtherEntity.getName() corresponds to #{name} in theOtherEntity.xml
 	public List<TheOtherEntity> selectByPojo(TheOtherEntity theOtherEntity);
 	
 	public void insert(TheOtherEntity theOtherEntity);
